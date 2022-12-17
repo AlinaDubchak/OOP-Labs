ea4f120cec5fea1da096b1704e17a370dfb4e3a7
Alina Dubchak <alina.dubchak.ev@gmail.com>
2022-12-17 02:59:29 +0200

    Add project files.

---
+++ Resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by RGR.rc
+
+
+
+#define IDC_MY_TOOLBAR					1
+#define ID_TOOL_DOT					    1
+#define ID_TOOL_LINE					2
+#define IDC_MYICON                      2
+#define ID_TOOL_RECT					3
+#define ID_TOOL_ELLIPSE					4
+#define ID_TOOL_RGB					5
+#define ID_TOOL_CONTRAST					6
+#define ID_TOOL_SHADOW	            7
+#define ID_TOOL_LIGHT                   8	
+#define IDC_MYICON                      2
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_RGR_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_RGR			107
+#define IDI_SMALL				108
+#define IDC_RGR			109
+#define IDC_MYICON				2
+#define IDC_STATIC				-1
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
---
+++ ToolBar.cpp
@@ -0,0 +1,109 @@
+#include "framework.h"
+#include "toolbar.h"
+#include "resource.h"
+#include "resource1.h"
+#include <commctrl.h>
+
+void ToolBar::OnCreate(HWND hWnd, HINSTANCE hInst)
+{
+    TBBUTTON tbb[8];
+
+    ZeroMemory(tbb, sizeof(tbb)); //обнулення пам'яті.
+    tbb[0].iBitmap = 0;
+    tbb[0].fsState = TBSTATE_ENABLED;
+    tbb[0].fsStyle = TBSTYLE_BUTTON; //тип елементу - кнопка
+    tbb[0].idCommand = ID_TOOL_DOT; //цей ID буде у повідомленні WM_COMMAND
+
+    tbb[1].iBitmap = 1;
+    tbb[1].fsState = TBSTATE_ENABLED;
+    tbb[1].fsStyle = TBSTYLE_BUTTON;
+    tbb[1].idCommand = ID_TOOL_LINE;
+
+    tbb[2].iBitmap = 2;
+    tbb[2].fsState = TBSTATE_ENABLED;
+    tbb[2].fsStyle = TBSTYLE_BUTTON;
+    tbb[2].idCommand = ID_TOOL_RECT;
+
+    tbb[3].iBitmap = 3;
+    tbb[3].fsState = TBSTATE_ENABLED;
+    tbb[3].fsStyle = TBSTYLE_BUTTON;
+    tbb[3].idCommand = ID_TOOL_ELLIPSE;
+
+    tbb[4].iBitmap = 4;
+    tbb[4].fsState = TBSTATE_ENABLED;
+    tbb[4].fsStyle = TBSTYLE_BUTTON;
+    tbb[4].idCommand = ID_TOOL_RGB;
+
+    tbb[5].iBitmap = 5;
+    tbb[5].fsState = TBSTATE_ENABLED;
+    tbb[5].fsStyle = TBSTYLE_BUTTON;
+    tbb[5].idCommand = ID_TOOL_CONTRAST;
+
+    tbb[6].iBitmap = 6;
+    tbb[6].fsState = TBSTATE_ENABLED;
+    tbb[6].fsStyle = TBSTYLE_BUTTON;
+    tbb[6].idCommand = ID_TOOL_SHADOW;
+
+    tbb[7].iBitmap = 7;
+    tbb[7].fsState = TBSTATE_ENABLED;
+    tbb[7].fsStyle = TBSTYLE_BUTTON;
+    tbb[7].idCommand = ID_TOOL_LIGHT;
+
+    SendMessage(hwndToolBar, TB_ADDBUTTONS, 8, (LPARAM)&tbb);
+
+    hwndToolBar = CreateToolbarEx(hWnd, //батьківське вікно
+        WS_CHILD | WS_VISIBLE | WS_BORDER | WS_CLIPSIBLINGS | CCS_TOP |
+        TBSTYLE_TOOLTIPS,
+        IDC_MY_TOOLBAR, //ID дочірнього вікна Toolbar
+        8, //кількість зображень у BITMAP
+        hInst,
+        IDB_BITMAP1, //ID ресурсу BITMAP
+        tbb,
+        8, //кількість кнопок (разом з роздільником)
+        24, 24, 24, 24, //розміри кнопок та зображень BITMAP
+        sizeof(TBBUTTON));
+};
+void ToolBar::PressBtn(HWND hWnd, LPARAM lParam)
+{
+    if (oldlParam)
+    {
+        SendMessage(hwndToolBar, TB_PRESSBUTTON, oldlParam, 0);
+    }
+    SendMessage(hwndToolBar, TB_PRESSBUTTON, lParam, 1);
+    oldlParam = lParam;
+}
+//---обробник повідомлення WM_SIZE---
+void ToolBar::OnSize(HWND hWnd)
+{
+    RECT rc, rw;
+    if (hwndToolBar)
+    {
+        GetClientRect(hWnd, &rc); //нові розміри головного вікна
+        GetWindowRect(hwndToolBar, &rw); //нам потрібно знати висоту Toolbar
+        MoveWindow(hwndToolBar, 0, 0, rc.right - rc.left, rw.bottom - rw.top, FALSE);
+    }
+}
+void ToolBar::OnNotify(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    LPNMHDR pnmh = (LPNMHDR)lParam;
+    if (pnmh->code == TTN_NEEDTEXT)
+    {
+        LPTOOLTIPTEXT lpttt = (LPTOOLTIPTEXT)lParam;
+        switch (lpttt->hdr.idFrom)
+        {
+        case ID_TOOL_DOT:
+            lstrcpy(lpttt->szText, L"Крапка");
+            break;
+        case ID_TOOL_LINE:
+            lstrcpy(lpttt->szText, L"Лінія");
+            break;
+        case ID_TOOL_RECT:
+            lstrcpy(lpttt->szText, L"Прямокутник");
+            break;
+        case ID_TOOL_ELLIPSE:
+            lstrcpy(lpttt->szText, L"Еліпс");
+            break;
+        default: lstrcpy(lpttt->szText, L"Щось невідоме");
+        }
+    }
+}
